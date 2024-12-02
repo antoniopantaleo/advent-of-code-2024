32507c92cdce3cb594cced13b89d69b28c2fe60e
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-12-02 13:57:37 +0100

    feat(day2): part 2

---
+++ 2/part2.py
@@ -0,0 +1,35 @@
+DEMO = False
+
+input = "demo1.txt" if DEMO else "input.txt"
+reports = open(input).readlines()
+reports = [[int(y) for y in x.split()] for x in reports]
+
+result = 0
+
+def isValidReport(report):
+    decreasing = all(report[i] > report[i+1] for i in range(len(report)-1))
+    increasing = all(report[i] < report[i+1] for i in range(len(report)-1))
+    monotone = decreasing or increasing
+    if not monotone:
+        return False
+    for idx, number in enumerate(report[1:]):
+        difference = number - report[idx]
+        if abs(difference) > 3:
+            return False
+    return True
+
+def canBeValid(report):
+    for idx in range(len(report)):
+        modified = report.copy() 
+        modified.pop(idx)
+        if isValidReport(modified):
+            return True
+    return False
+
+for report in reports:
+    if isValidReport(report):
+        result += 1
+    else:
+        if canBeValid(report):
+            result += 1
+print(result)
